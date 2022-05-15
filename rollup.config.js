a402479a89f477fd50ce52b89e9be07e017842e7
袁首京 <yuanshoujing@gmail.com>
2022-05-15 11:56:36 +0800

    config

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,18 @@
+import { terser } from "rollup-plugin-terser";
+import commonjs from "@rollup/plugin-commonjs";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+
 export default {
   input: "src/index.js",
 
   output: {
     dir: "dist",
     format: "umd",
+    name: "grit",
+    sourcemap: true,
+    plugins: [terser()],
   },
+
+  plugins: [nodeResolve(), commonjs(), babel({ babelHelpers: "bundled" })],
 };
