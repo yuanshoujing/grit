ab79e33a09ff22441f2d19fc06ebfb3fed1023e1
袁首京 <yuanshoujing@gmail.com>
2022-05-23 16:41:28 +0800

    build

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,8 @@ import { terser } from "rollup-plugin-terser";
 import commonjs from "@rollup/plugin-commonjs";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
+import del from "rollup-plugin-delete";
+import copy from "rollup-plugin-copy";
 
 export default {
   input: "src/index.js",
@@ -14,7 +16,20 @@ export default {
     plugins: [terser()],
   },
 
-  plugins: [nodeResolve(), commonjs(), babel({ babelHelpers: "runtime" })],
+  plugins: [
+    del({ targets: "dist/*" }),
+    nodeResolve(),
+    commonjs(),
+    babel({ babelHelpers: "runtime" }),
+    copy({
+      targets: [
+        {
+          src: "package.json",
+          dest: "dist/package.json",
+        },
+      ],
+    }),
+  ],
 
   external: ["lodash", /@babel\/runtime/],
 };
