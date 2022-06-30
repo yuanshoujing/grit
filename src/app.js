d2860bbda246d709c017c1a329ad429e30b2c366
袁首京 <yuanshoujing@gmail.com>
2022-06-30 18:04:46 +0800

    findHandles

--- src/app.js
+++ src/app.js
@@ -5,17 +5,24 @@ import { RouteEvents } from "./router";
 
 const log = debug("niba:app");
 
-function findHandler(routes, route) {
-  for (const [path, value] of Object.entries(routes)) {
-    if (route.indexOf(path) !== 0) {
-      continue;
-    }
+function findHandles(routes, route, result = []) {
+  const r = routes.find(({ path }) => {
+    return route.indexOf(path) >= 0;
+  });
 
-    const sub_route = route.substring(path.length);
-    findHandler(routes, sub_route);
-
-    // TODO: 递归返回值
+  if (!r) {
+    return;
   }
+
+  const { path, view, children } = r;
+  result.push(view);
+
+  const sub_route = route.substring(path.length);
+  if (!sub_route) {
+    return;
+  }
+
+  return findHandles(children, sub_route, result);
 }
 
 function create(mnt, routes = {}) {
@@ -24,15 +31,14 @@ function create(mnt, routes = {}) {
   RouteEvents.on(EventRouteChanged, (route) => {
     logger.info("--> route: ", route);
     const { state, search, from, to } = route;
-    findHandler(routes, to);
+    const handles = [];
+    findHandles(routes, to, handles);
+
+    // TODO: 依次挂载 handles，已挂载过的不再次挂载？
 
     mnt.innerHTML = "";
 
-    for (const [target, handle] of Object.entries(routes)) {
-      if (to !== target) {
-        continue;
-      }
-
+    for (const view of handles) {
       (async () => {
         const obj = await handle();
         if (_.has(obj, "root") && _.has(obj, "mount")) {
